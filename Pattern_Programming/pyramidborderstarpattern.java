2d77b0aac9d0b52943c42166595cd8018a2564f0
SniperNikhil <nikhilmahendrakar02@gmail.com>
2025-10-18 12:29:28 +0530

    Completed 115 problems

---
+++ Pattern_Programming/pyramidborderstarpattern.java
@@ -0,0 +1,40 @@
+package Pattern_Programming;
+
+/* print below pattern
+
+    *
+   * *
+  *   *
+ *     *
+*********
+
+*/
+
+public class pyramidborderstarpattern {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+
+            for (int j = i; j < 5; j++) {
+
+                System.out.print(" ");
+
+            }
+
+            for (int j = 1; j <= 2 * i - 1; j++) {
+
+                if (j == 1 || i == 5 || j == 2 * i - 1) {
+
+                    System.out.print("*");
+
+                } else {
+
+                    System.out.print(" ");
+                }
+
+            }
+
+            System.out.println();
+
+        }
+    }
+}
